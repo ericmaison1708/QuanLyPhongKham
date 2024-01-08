8eca4975a281ccd864bbe24d3ea791fbf896f5ea
ericmaison1708 <ericmaison17@gmail.com>
2024-01-08 22:54:48 +0700

    a

--- FrmDangNhap.cs
+++ FrmDangNhap.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.XtraEditors;
+using Quanlyphongkham;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,11 +9,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace QuanLyPhongKham
 {
     public partial class FrmDangNhap : DevExpress.XtraEditors.XtraForm
     {
+        ConnectToSQL con = new ConnectToSQL();
         public FrmDangNhap()
         {
             InitializeComponent();
@@ -20,9 +23,19 @@ namespace QuanLyPhongKham
 
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
-            FrmMain f = new FrmMain();
-            this.Hide();
-            f.ShowDialog();
+            DataTable dt = new DataTable();
+            dt = con.GetData("SELECT * FROM TaiKhoan where TenDangNhap = '" + txtTaiKhoan.Text + "' and MatKhau ='" + txtMatKhau.Text + "'");
+            if(dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Đăng nhập thành công", "Chúc mừng", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                FrmMain f = new FrmMain(dt.Rows[0][0].ToString(), dt.Rows[0][1].ToString(), dt.Rows[0][2].ToString());
+                this.Hide();
+                f.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Thông tin tài khoản không chính xác", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QuanLyPhongKham.Properties
-{
-
-
+namespace QuanLyPhongKham.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ERICMAISON\\SQLEXPRESS;Initial Catalog=QUANLYPHONGKHAM;Persist Securit" +
+            "y Info=True;User ID=sa;Password=soneric17082003")]
+        public string QUANLYPHONGKHAMConnectionString {
+            get {
+                return ((string)(this["QUANLYPHONGKHAMConnectionString"]));
             }
         }
     }
